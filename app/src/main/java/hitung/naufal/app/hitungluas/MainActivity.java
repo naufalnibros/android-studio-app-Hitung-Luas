36b5899192e44be28f4fe545416553819b666d22
naufalnibros <masnaufal05@gmail.com>
2016-11-15 21:29:07 +0700

    App Hitung Luas V.1

---
+++ app/src/main/java/hitung/naufal/app/hitungluas/MainActivity.java
@@ -0,0 +1,41 @@
+package hitung.naufal.app.hitungluas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText edtPanjang, edtLebar;
+    private Button btnHitung;
+    private TextView txtLuas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Hitung Luas Persegi Panjang");
+
+        edtPanjang = (EditText)findViewById(R.id.edt_panjang);
+        edtLebar = (EditText)findViewById(R.id.edt_lebar);
+        btnHitung = (Button)findViewById(R.id.btn_hitung);
+        txtLuas = (TextView)findViewById(R.id.txt_luas);
+
+        btnHitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String panjang = edtPanjang.getText().toString().trim();
+                String lebar = edtLebar.getText().toString().trim();
+
+                double p = Double.parseDouble(panjang);
+                double l = Double.parseDouble(lebar);
+
+                double luas = p * l;
+
+                txtLuas.setText("Luas : "+luas);
+            }
+        });
+    }
+}
